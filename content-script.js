0ccf1b91db604b8f63e3a03ae602c2bae98c4c5b
Ryan Odd <ryan@stageten.tv>
2024-08-20 20:37:12 -0400

    Target pencil button in all states

--- content-script.js
+++ content-script.js
@@ -1,11 +1,28 @@
+// returns pencil button HTMLButtonElement or null
+function getPencilButton() {
+  const pencilButtonIconInactive = document.querySelector(".xwd__toolbar_icon--pencil")
+  const pencilButtonIconActive = document.querySelector(".xwd__toolbar_icon--pencil-active")
+  const pencilButtonAcrostic = document.querySelector(".acrostic-tool__pencil") // inactive or active
+  let pencilButton = null
+  if (pencilButtonIconInactive) {
+    pencilButton = pencilButtonIconInactive.closest("button")
+  }
+  if (pencilButtonIconActive) {
+    pencilButton = pencilButtonIconActive.closest("button")
+  }
+  if (pencilButtonAcrostic) {
+    pencilButton = pencilButtonAcrostic.closest("button")
+  }
+  return pencilButton ?? null
+}
+
 function togglePencilButton() {
-  const pencilButtonIcon = document.querySelector(".xwd__toolbar_icon--pencil")
-  const pencilButton = pencilButtonIcon.closest("button")
-  pencilButton.click()
+  const pencilButton = getPencilButton()
+  pencilButton?.click()
 }
 
 function onKeyPress(event) {
-  if (event.code === "LeftShift") {
+  if (event.code === "ShiftLeft") {
     togglePencilButton()
   }
 }
